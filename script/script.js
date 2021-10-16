2cdfb718cfec2bbdf259a09ad68cf29c7a880802
Shaunak Basu <basushaunak@msn.com>
2021-10-16 10:53:34 +0530

    Logic mostly done, need to resolve the issue with multiple action key press in succession

--- script/script.js
+++ script/script.js
@@ -14,6 +14,7 @@ let tmpNumArray = [];
 let lastNum = "";
 let currNum = "";
 let lastKey = "";
+let lastOperator = "";
 let operator = "";
 const numberKeys = document.querySelectorAll(".num-key");
 const actionKeys = document.querySelectorAll(".action-key");
@@ -22,6 +23,11 @@ function keyPress(event){
     let keyCode = event.keyCode;
     if(keyCode === 13){
         keyCode = 61;
+    }
+    if(keyCode === 27){
+        initCalculator();
+        refreshDisplay();
+        return;
     }
     let btnPressed = document.querySelector(`button[data-key="${keyCode}"]`);
     if(!btnPressed){
@@ -44,10 +50,95 @@ function mouseClick(event){
         currNum = tmpNumArray.join("");
         refreshDisplay();
     }else{
-        alert ("pressed operator");
+        switch(currentButton){
+            case "-":
+                lastOperator=operator;
+                operator = "-";
+                break;
+            case "+":
+                lastOperator=operator;
+                operator = "+";
+                break;
+            case "÷":
+                lastOperator=operator;
+                operator = "÷";
+                break;
+            case "X":
+                lastOperator=operator;
+                operator = "X";
+                break;
+            case "=" :
+                lastOperator=operator;
+                operator = "=";
+                break;
+            default:
+                console.log("Logic Problem. Should not be here!");
+                console.log("Button Pressed: " + currentButton);
+                break;
+        };
+        markOperator(operator);
+        if(lastNum === ""){
+            lastNum = currNum;
+            currNum = 0;
+            tmpNumArray = [];
+            refreshDisplay();
+        } else{
+            currNum = getResult(lastNum, currNum, lastOperator);
+            lastOperator = operator === "=" ? "": operator;
+            lastNum = "";
+            tmpNumArray=[];    
+            refreshDisplay();
+        }        
     }
 };
 
+function markOperator(ops){
+    let tmpArray = Array.from(actionKeys);
+    tmpArray.forEach(item =>{
+        if(item.textContent === ops){
+            item.style.color = "#FF0000";
+        } else {
+            item.style.color = "#FFFFFF";
+        }
+        
+    })
+}
+
 function refreshDisplay(){
     document.querySelector("#calculator-display").textContent=currNum;
+}
+
+function initCalculator(){
+    numArray = [];
+    numArrayIdx = 0;
+    tmpNumArray = [];
+    lastNum = "";
+    currNum = "";
+    lastKey = "";
+    lastOperator = "";
+    operator = "";
+}
+
+function getResult(num1, num2, operator){
+    let result = 0;
+    switch (operator){
+        case "-":
+            result = (+num1) - (+num2);
+            break;
+        case "÷":
+            result = (+num1) / (+num2)
+            break;
+        case "+":
+            result = (+num1) + (+num2)
+            break;
+        case "X":
+            result = (+num1) * (+num2)
+            break;
+        default:
+            console.log("Logic Problem. LastOperator is not valid");
+            console.log("Last Operator: "+lastOperator);
+            return NaN;
+    }
+    alert(result);
+    return ("" + result);
 }
